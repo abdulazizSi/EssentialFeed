a7470ce3fcc550753781bee90dac642f9f96930c
Abdulaziz ALSIKH <Abdulaziz.si.aa@gmail.com>
2025-01-04 17:44:45 +0300

    'RemoteFeedLoader' does not request data upon creattion

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  EssentialFeedTests
+//
+//  Created by Abdulaziz Alsikh on 4.01.2025.
+//
+
+import Testing
+
+struct RemoteFeedLoaderTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
