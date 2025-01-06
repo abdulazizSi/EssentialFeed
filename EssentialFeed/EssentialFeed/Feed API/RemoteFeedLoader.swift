421382f8c2400fb2319ff80ae96885daa279c6e2
Abdulaziz ALSIKH <Abdulaziz.si.aa@gmail.com>
2025-01-06 10:50:56 +0300

    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public typealias HTTPClientResponse = Result<HTTPURLResponse, Error>
+public typealias HTTPClientResponse = Result<(Data, HTTPURLResponse), Error>
 
 public protocol HTTPClient {
     func get(from url: URL, completion: @escaping (HTTPClientResponse) -> Void)
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -64,6 +64,19 @@ class RemoteFeedLoaderTest: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0)}
+        
+        let invalidJSON = Data("Invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     //MARK: - Helper
     
     private func makeSUT(url: URL = URL(string: "https://example.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -88,7 +101,7 @@ class RemoteFeedLoaderTest: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
@@ -96,7 +109,7 @@ class RemoteFeedLoaderTest: XCTestCase {
                 headerFields: nil
             )!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
