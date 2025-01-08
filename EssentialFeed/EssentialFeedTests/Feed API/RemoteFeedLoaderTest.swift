81051216f73600f0b6c0cbe98247fd68b8227f6c
Abdulaziz ALSIKH <Abdulaziz.si.aa@gmail.com>
2025-01-08 17:08:13 +0300

    Remove 'LoadFeedResult' Equatable conformance from production since it's a test-only requirement

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
@@ -164,17 +164,31 @@ class RemoteFeedLoaderTest: XCTestCase {
     
     private func expect(
         _ sut: RemoteFeedLoader,
-        toCompleteWith result: RemoteFeedLoader.Result,
+        toCompleteWith expectedResult: RemoteFeedLoader.Result,
         when action: () -> Void,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems)
+                
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError)
+                
+            default:
+                XCTFail()
+            }
+            
+            exp.fulfill()
+        }
         
         action()
         
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class HTTPClientSpy: HTTPClient {
